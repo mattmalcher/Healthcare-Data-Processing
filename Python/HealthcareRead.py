e4ce2c2ba9bbfb5bef0af7277ddcec0820424492
mattmalcher <matmalcher@gmail.com>
2017-09-27 13:37:08 +0100

    Initial Commit

---
+++ Python/HealthcareRead.py
@@ -0,0 +1,15 @@
+# Script for scraping & compiling healthcare data from excel files
+
+# Module Imports
+
+
+# Generate list of valid input files
+
+
+# Import Function
+
+
+# Processing
+
+
+# Compile & Output
